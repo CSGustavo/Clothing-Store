1e262176fcde5654964a4fb436b0feb53f2ddec7
JavierCharris <molinaenrique@unbosque.edu.co>
2021-09-17 15:26:36 -0500

    DAO

---
+++ src/main/java/com/tienda/mintic/dao/UsuariosDAO.java
@@ -0,0 +1,10 @@
+package com.tienda.mintic.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.tienda.mintic.model.Usuarios;
+
+public interface UsuariosDAO extends JpaRepository<Usuarios, Integer>{
+
+	
+	
+}
